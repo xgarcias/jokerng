44e67831d1e9ef0581ade1ae10ad8a54c46dcc16
Xavier Garcia <xavi.garcia@gmail.com>
2018-04-21 22:04:27 +0200

    Basic setup to package ezjail basejails and jails

--- packer/shared/packer/scripts/lo1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-sysrc cloned_interfaces+=" lo1"
-sysrc ifconfig_lo1="up"
-sysrc ifconfig_lo1="fib 15 172.31.255.200/24 group natted description 'natted jails'"
-sysrc ifconfig_lo1_aliases="inet 172.31.255.201-206/32"
-ifconfig lo1 create
